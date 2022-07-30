10dde669eaee8feb69e90fc8af257d4595920f1d
Kundan <kundan kamble>
2022-07-30 15:04:07 -0700

    Added Single Player Start Position program

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+     public class Program
+     {
+        static void Main(string[] args)
+        {
+            Snake.SinglePlayer();
+        }
+     }
+}
