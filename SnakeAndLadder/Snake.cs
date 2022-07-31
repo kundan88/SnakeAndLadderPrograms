b25b957e4e82530b1219d21d588b66e5321f7d4f
Kundan <kundan kamble>
2022-07-30 22:29:10 -0700

    Added Exact Winning program

--- SnakeAndLadder/Snake.cs
+++ SnakeAndLadder/Snake.cs
@@ -30,7 +30,15 @@ namespace SnakeAndLadder
                         break;
 
                     case 1:
-                        position += rollDice;
+
+                        if (position != 100 && position < 100)
+                        {
+                            int currentPos = position + rollDice;
+                            if (currentPos <= 100)
+                            {
+                                position += rollDice;
+                            }
+                        }
                         Console.WriteLine("Player moved to {0} position", position);
                         break;
 
