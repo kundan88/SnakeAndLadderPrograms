1a5373fbc96dcd65c0ad2ad2bd1c017a12a5dca4
Kundan <kundan kamble>
2022-07-30 22:19:35 -0700

    Added Repeat till the player reaches program

--- SnakeAndLadder/Snake.cs
+++ SnakeAndLadder/Snake.cs
@@ -14,32 +14,39 @@ namespace SnakeAndLadder
             Random random = new Random();
         }
 
-        public static void rollDice() 
+        public static void rollDice()
         {
             int position = 0;
-            int checkOption = new Random().Next(0, 3);
-            int rollDice = RollDice();
-            Console.WriteLine("PLayer got {0} on dice", rollDice);
-
-            switch (checkOption)
+            while (position != 100 && position <= 100)
             {
-                case 0:
-                    position += 0;
-                    Console.WriteLine("Player stayed at {0} position", position);
-                    break;
-                case 1:
-                    position += rollDice;
-                    Console.WriteLine("Player moved to {0} position", position);
-                    break;
-                case 2:
-                    if (position >= rollDice)
-                    {
-                        position -= rollDice;
-                    }
-                    Console.WriteLine("Player moved back to {0} position", position);
-                    break;
-            }
+                int rollDice = RollDice();
+                Console.WriteLine("PLayer got {0} on dice", rollDice);
+                int checkOption = new Random().Next(0, 3);
+                switch (checkOption)
+                {
+                    case 0:
+                        position += 0;
+                        Console.WriteLine("Player stayed at {0} position", position);
+                        break;
 
+                    case 1:
+                        position += rollDice;
+                        Console.WriteLine("Player moved to {0} position", position);
+                        break;
+
+                    case 2:
+                        if (position >= rollDice)
+                        {
+                            position -= rollDice;
+                        }
+                        else
+                        {
+                            position = 0;
+                        }
+                        Console.WriteLine("Player moved back to {0} position", position);
+                        break;
+                }
+            }
 
         }
     }
